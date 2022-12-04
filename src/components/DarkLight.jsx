6f44a2474bd37630e70b579fca9dfc30b6268647
bilalshahzadd <bilalshahzad521@gmail.com>
2022-12-04 07:14:43 +0500

    navbar update

--- src/components/DarkLight.jsx
+++ src/components/DarkLight.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function DarkLight(props) {
 
+    const [btnText, setBtnText] = useState("Toggle Light Mode")
+
     const toggleDarkMode = () => {
         document.body.classList.toggle("light-mode");
+        if (btnText == "Toggle Light Mode") {
+            setBtnText("Toggle Dark Mode");
+        } else {
+            setBtnText("Toggle Light Mode");
+        }
     }
 
     return (
         <>
-            <button className={props.className} onClick={toggleDarkMode}>Dark/Light</button>
+            <button className={props.className} onClick={toggleDarkMode}>{btnText}</button>
         </>
     )
 }
