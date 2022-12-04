0f3932429a67e2286c81f8b346e01f164d1d12e9
bilalshahzadd <bilalshahzad521@gmail.com>
2022-12-04 21:59:38 +0500

    responsive update

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -19,6 +19,9 @@ export default function TextForm(props) {
 
     const handleOnChange = (event) => {
         setText(event.target.value);
+        setCopyText({
+            visibility: "hidden"
+        })
     }
 
     const handleClear = () => {
