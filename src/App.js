ec55f63b05f548bffd117a9e8c5167e8f05eda74
bilalshahzadd <bilalshahzad521@gmail.com>
2022-12-04 07:22:07 +0500

    navbar update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import "./App.css"
-// import TextForm from "./components/TextForm";
+import TextForm from "./components/TextForm";
 import Nav from "./components/Nav";
-import About from "./components/About";
 
 function App() {
 
   return (
     <>
       <Nav />
-      <About />
+      <TextForm />
     </>
   );
 }
