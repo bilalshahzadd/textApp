2dc5b4ea363aa4519b69b089ebd4f09c54ba2fe6
bilalshahzadd <bilalshahzad521@gmail.com>
2022-12-04 20:19:39 +0500

    responsive update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -19,6 +19,7 @@ export default function Nav() {
 
             <header className="mobileHeader">
                 <h1 className="mobileTitle">TextApp</h1>
+                <button className="fa-solid fa-bars"></button>
             </header>
 
         </>
